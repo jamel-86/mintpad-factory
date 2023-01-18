465872a92c15f14bed130c7527343827b124cad5
SolidityOnTheBlock <73886791+jamel-86@users.noreply.github.com>
2023-01-18 20:48:53 +0700

    Add files via upload

---
+++ Mintpad.sol
@@ -0,0 +1,180 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.11 <0.9.0;
+
+import 'erc721a/contracts/extensions/ERC721AQueryable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
+
+contract Collection is ERC721AQueryable, Ownable, ReentrancyGuard {
+
+  using Strings for uint256;
+
+  bytes32 public merkleRoot;
+  mapping(address => bool) public whitelistClaimed;
+
+  string public uriPrefix = '';
+  string public uriSuffix = '.json';
+  string public hiddenMetadataUri;
+  address public mintpadFeeAddress;
+  uint256 public mintpadFee = 6;
+  
+  uint256 public cost;
+  uint256 public maxSupply;
+  uint256 public maxMintAmountPerTx;
+
+  bool public paused = true;
+  bool public whitelistMintEnabled = false;
+  bool public revealed = false;
+  address public creator;
+
+  constructor(
+    string memory _tokenName,
+    string memory _tokenSymbol,
+    uint256 _cost,
+    uint256 _maxSupply,
+    uint256 _maxMintAmountPerTx,
+    string memory _hiddenMetadataUri,
+    address payable _creator,
+    address _mintpadFeeAddress
+  ) ERC721A(_tokenName, _tokenSymbol) {
+    setCost(_cost);
+    maxSupply = _maxSupply;
+    setMaxMintAmountPerTx(_maxMintAmountPerTx);
+    setHiddenMetadataUri(_hiddenMetadataUri);
+    _transferOwnership(tx.origin);
+    creator = _creator;
+    mintpadFeeAddress = _mintpadFeeAddress;
+  }
+
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, 'Invalid mint amount!');
+    require(totalSupply() + _mintAmount <= maxSupply, 'Max supply exceeded!');
+    _;
+  }
+
+  modifier mintPriceCompliance(uint256 _mintAmount) {
+    require(msg.value >= cost * _mintAmount, 'Insufficient funds!');
+    _;
+  }
+
+  function whitelistMint(uint256 _mintAmount, bytes32[] calldata _merkleProof) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
+    // Verify whitelist requirements
+    require(whitelistMintEnabled, 'The whitelist sale is not enabled!');
+    require(!whitelistClaimed[_msgSender()], 'Address already claimed!');
+    bytes32 leaf = keccak256(abi.encodePacked(_msgSender()));
+    require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), 'Invalid proof!');
+
+    whitelistClaimed[_msgSender()] = true;
+    _safeMint(_msgSender(), _mintAmount);
+  }
+
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) mintPriceCompliance(_mintAmount) {
+    require(!paused, 'The contract is paused!');
+
+    _safeMint(_msgSender(), _mintAmount);
+  }
+  
+  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+    _safeMint(_receiver, _mintAmount);
+  }
+
+  function _startTokenId() internal view virtual override returns (uint256) {
+    return 1;
+  }
+
+  function tokenURI(uint256 _tokenId) public view virtual override(ERC721A, IERC721A) returns (string memory) {
+    require(_exists(_tokenId), 'ERC721Metadata: URI query for nonexistent token');
+
+    if (revealed == false) {
+      return hiddenMetadataUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
+        : '';
+  }
+
+  function setRevealed(bool _state) public onlyOwner {
+    revealed = _state;
+  }
+
+  function setCost(uint256 _cost) public onlyOwner {
+    cost = _cost;
+  }
+
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
+  }
+
+  function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
+  }
+
+  function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+    uriPrefix = _uriPrefix;
+  }
+
+  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+    uriSuffix = _uriSuffix;
+  }
+
+  function setPaused(bool _state) public onlyOwner {
+    paused = _state;
+  }
+
+  function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+    merkleRoot = _merkleRoot;
+  }
+
+  function setWhitelistMintEnabled(bool _state) public onlyOwner {
+    whitelistMintEnabled = _state;
+  }
+
+  function withdraw() public onlyOwner nonReentrant {
+    // This will pay MintPad DAO 6% of the initial sale.
+    // =============================================================================
+    (bool hs, ) = payable(mintpadFeeAddress).call{value: address(this).balance * mintpadFee / 100}('');
+    require(hs);
+    // =============================================================================
+
+    // This will transfer the remaining contract balance to the project owner.
+    // =============================================================================
+    (bool os, ) = payable(creator).call{value: address(this).balance}('');
+    require(os);
+    // =============================================================================
+    // This will transfer the ownership of the contract to the creator.
+    _transferOwnership(creator);
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return uriPrefix;
+  }
+}
+
+// Factory contract that creates the mint pool, to prevent creators of manipulating above contract!
+
+contract CollectionFactory {
+    address payable[] public deployedCollections;
+
+    function createCollection(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint256  _cost,
+        uint256  _maxSupply,
+        uint256  _maxMintAmountPerTx,
+        string memory _hiddenMetadataUri,
+        address payable _creator,
+        address _mintpadFeeAddress
+    ) public { 
+        address newCollection = address(new Collection(_tokenName, _tokenSymbol, _cost, _maxSupply, _maxMintAmountPerTx, _hiddenMetadataUri, _creator, _mintpadFeeAddress));
+        deployedCollections.push(payable(newCollection));
+    }
+
+    function getDeployedCollections() public view returns (address payable[] memory) {
+        return deployedCollections;
+    }
+}
